5210094973acb575ec0a67fbc554c31a6a59e4d7
Peter Mbugua <mbuguahptr@gmail.com.noreply.github.com>
2025-12-12 10:58:12 +0300

    Update render build script and ignore virtual environment

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
-set -o errexit
+set -o errexit  # Exit immediately if a command exits with a non-zero status
 
+# Upgrade pip
 pip install --upgrade pip
+
+# Install dependencies
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
-python manage.py migrate --noinput
-python manage.py create_admin
+# Collect static files
+python backend/manage.py collectstatic --noinput
+
+# Apply migrations
+python backend/manage.py migrate --noinput
